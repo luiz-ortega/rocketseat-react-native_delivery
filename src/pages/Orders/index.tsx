9995e498fedf64ab223ddec21180d58d54f888fd
luiz-ortega <luizngust@hotmail.com>
2020-10-06 19:31:29 -0300

    feat: list orders.

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,13 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      api.get('/orders').then(response => {
+        const formatedFoods = response.data.map((order: Food) => ({
+          ...order,
+          formattedPrice: formatValue(order.price),
+        }));
+        setOrders(formatedFoods);
+      });
     }
 
     loadOrders();
